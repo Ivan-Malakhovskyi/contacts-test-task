14a62e0dae5b5082d6f1ea542557aa6072b2c6e0
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2024-08-03 19:55:15 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+
 import App from "./App.jsx";
 import "./index.css";
 
